32bcd996521763547290e29123e27c62ec8c4297
hallelujahylefay <lefayy1@t410-du-21-06.org.aalto.fi>
2023-06-16 11:33:41 +0300

    test ito.

--- test/test_ito_strato.py
+++ test/test_ito_strato.py
@@ -2,21 +2,24 @@ from bayesian_ode_solver.ito_stratonovich import to_stratonovich, to_ito
 import numpy.testing as npt
 import jax.numpy as jnp
 
-def test_ito_strato_1d():
+def test_ito_strato_symmetry():
     drift = lambda x, t: 1.0
     diff = lambda x, t: x
-
     _drift, _diff = to_stratonovich(*to_ito(drift, diff))
-
     x = 0.5
     t = 1.0
 
     npt.assert_equal(_drift(x, t), jnp.array([drift(x, t)]))
 
-    drift = lambda x, t: 1.0
-    diff = lambda x, t: x
-    strat_drift, strat_diff = to_stratonovich(drift, diff)
-    npt.assert_equal(strat_drift(x, t), jnp.array([1.0 - x * 0.5]))
+    drift = lambda x, t: x
+    diff = lambda x, t: jnp.array([[x[0], 2 * x[1]],
+                                   [3 * x[0] + 4 * x[1], 5 * x[1]]])
+    tilde_drift, tilde_diff = to_stratonovich(*to_ito(drift, diff))
+    x = jnp.array([0.5, 0.9])
+    t = 1.0
+
+    ito_drift, _ = to_ito(drift, diff)
+    npt.assert_array_almost_equal(tilde_drift(x, t), drift(x, t))
 
 def test_ito_strato_diagonal():
     drift = lambda x, t: x
@@ -28,16 +31,21 @@ def test_ito_strato_diagonal():
     t = 1.0
     npt.assert_array_almost_equal(strat_drift(x, t), x - 0.5 * x)
 
-def test_ito_strato_symmetry():
+def test_ito_strato_1d():
+    drift = lambda x, t: 1.0
+    diff = lambda x, t: x
+    x = 0.5
+    t = 1.0
+    strat_drift, strat_diff = to_stratonovich(drift, diff)
+    npt.assert_equal(strat_drift(x, t), jnp.array([1.0 - x * 0.5]))
+
+def test_ito_strato_md():
     drift = lambda x, t: x
-    diff = lambda x, t: jnp.array([[x[0], 2*x[1]],
-                                   [3*x[0]+4*x[1], 5*x[1]]])
-
-    tilde_drift, tilde_diff = to_stratonovich(*to_ito(drift, diff))
-
+    diff = lambda x, t: jnp.array([[x[0], 2 * x[1]],
+                                   [3 * x[0] + 4 * x[1], 5 * x[1]]])
     x = jnp.array([0.5, 0.9])
     t = 1.0
-
     ito_drift, _ = to_ito(drift, diff)
-    print(ito_drift(x, t))
-    npt.assert_array_almost_equal(tilde_drift(x, t), drift(x, t))
+    npt.assert_array_almost_equal(ito_drift(x, t), drift(x,t) +
+                                  0.5*jnp.array([(x[0]*1 + 2*x[1]*0 + (3*x[0] + 4*x[1])*0 + 5*x[1]*2),
+                                                (x[0]*3+2*x[1]*0+(3*x[0]+4*x[1])*4+5*x[1]*5)]))
