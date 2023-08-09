91f6c9cc40484b4bbedc623f51bff3580e0cff90
hallelujahylefay <yvann.lefay@ensae.fr>
2023-08-09 16:37:40 +0300

    exp

--- scripts/local_order_experiment_from_scratch.py
+++ scripts/local_order_experiment_from_scratch.py
@@ -6,6 +6,7 @@ import jax.numpy as jnp
 from bayesian_sde_solver.foster_polynomial import get_approx
 from bayesian_sde_solver.foster_polynomial import get_approx_fine as _get_approx_fine
 from bayesian_sde_solver.ode_solvers import ekf0
+from bayesian_sde_solver.sde_solvers import euler_maruyama_pathwise
 
 fine_N = 1000
 get_approx_fine = partial(_get_approx_fine, N=fine_N)
@@ -16,8 +17,8 @@ solver = ekf0
 delta = 0.1
 
 
-def wrapped(_key, init, vector_field, T):
-    return solver(None, init=init, vector_field=vector_field, h=T, N=1)
+def wrapped(_key, init, vector_field, T, N=1):
+    return solver(None, init=init, vector_field=vector_field, h=T/N, N=N)
 
 
 # Integrated Brownian motion
@@ -37,7 +38,7 @@ init_x0 = jnp.ones((1,))
 
 
 def drift(x, t):
-    return x
+    return jnp.exp(x)
 
 
 def sigma(x, t):
@@ -60,10 +61,9 @@ def customized_solver(h, M, coeffs):
     return ts, samples
 
 
-def experiment(delta):
+def experiment(delta, N):
     keys = jax.random.split(JAX_KEY, 1000)
     fine_delta = delta / fine_N
-
     @jax.vmap
     def local_step(key_OP):
         key = jax.random.split(key_OP, 1)[0]
@@ -75,9 +75,9 @@ def experiment(delta):
         BM = jnp.cumsum(fine_incs, axis=0) # computing the corresponding BM
 
         fine_incs *= jnp.sqrt(1 / fine_delta)  # normalizing them to var. 1, needed for euler_maruyama_pathwise
-        # _, X_DELTA_EULER = euler_maruyama_pathwise(fine_incs, init=init_x0, drift=drift, sigma=sigma, h=fine_delta,
-        #                                           N=fine_N)  # solving using EM scheme this path
-        # X_DELTA_EULER = X_DELTA_EULER[-1]  # taking last value, corresponding to X^{eu}_{delta} with h = fine_delta
+        _, X_DELTA_EULER = euler_maruyama_pathwise(fine_incs, init=init_x0, drift=drift, sigma=sigma, h=fine_delta,
+                                                   N=fine_N)  # solving using EM scheme this path
+        X_DELTA_EULER = X_DELTA_EULER[-1]  # taking last value, corresponding to X^{eu}_{delta} with h = fine_delta
 
 
         # in case we know the closed formula:
@@ -85,7 +85,7 @@ def experiment(delta):
             return init_x0 * jnp.exp(t/2+b)
         def closed_formula(b, t):
             return jnp.exp(t) * (init_x0 + b)
-        X_DELTA_FINE_CLOSED_FORMULA = closed_formula(BM[-1], delta)
+        # X_DELTA_FINE_CLOSED_FORMULA = closed_formula(BM[-1], delta)
 
         # solving using a chosen ODE solver (see def of customized_solver), the Nfine ODE => less than order 1.0
         # obtain 0.5.., both using EKF0 or euler
@@ -95,7 +95,7 @@ def experiment(delta):
         # solve one step of EKF0 for the greater parabola ODE.
         func = lambda t: eval_parabola(t, delta, *coeffs)
         vector_field = lambda x, t: drift(x, t) + sigma(x, t) @ jax.jacfwd(func)(t)
-        X_DELTA_EKF0_PARABOLAS = wrapped(None, init_x0, vector_field, delta)
+        X_DELTA_EKF0_PARABOLAS = wrapped(None, init_x0, vector_field, delta, N=N)
 
         # in case we know the ode closed formula:
         def closed_formula_ode(w, i, t):
@@ -103,17 +103,17 @@ def experiment(delta):
         def closed_formula_ode(w, i, t):
             return jnp.exp(t / 2 + w / delta * t + t / delta * (t / delta - 1) * jnp.sqrt(6) * i)
 
-        X_DELTA_FINE_CLOSED_ODE_FORMULA = closed_formula_ode(coeffs[0], coeffs[1], delta)
+        # X_DELTA_FINE_CLOSED_ODE_FORMULA = closed_formula_ode(coeffs[0], coeffs[1], delta)
 
 
         # solving the greater parabola ODE using euler with chosen number of steps => less than order 1.0
         # X_DELTA_EULER_PARABOLAS = euler(init_x0, vector_field, h=delta/10, N=10) #0.5
 
-        return X_DELTA_EKF0_PARABOLAS, X_DELTA_FINE_CLOSED_FORMULA
+        return X_DELTA_EKF0_PARABOLAS, X_DELTA_EULER
 
     return local_step(keys)
 
 
 deltas = [0.1, 0.05, 0.025, 0.01]
 for delta in deltas:
-    jnp.save(f'experiment_{delta}', experiment(delta))
+    jnp.save(f'experiment_{delta}', experiment(delta, int(1/delta**0)))
