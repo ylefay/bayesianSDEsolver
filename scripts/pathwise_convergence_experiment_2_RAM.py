387e9eb63e26c4c1f0eec170377d8a1dbbb82d4d
hallelujahylefay <yvann.lefay@ensae.fr>
2023-08-17 11:35:59 +0300

    attempt to optimize experiments for RAM

---
+++ scripts/pathwise_convergence_experiment_2_RAM.py
@@ -0,0 +1,164 @@
+from functools import partial
+
+import jax
+import jax.numpy as jnp
+
+from bayesian_sde_solver.foster_polynomial import get_approx_fine as _get_approx_fine
+from bayesian_sde_solver.ito_stratonovich import to_stratonovich
+from bayesian_sde_solver.ode_solvers import ekf0_2, ekf1_2, ekf0
+from bayesian_sde_solver.sde_solver import sde_solver
+from bayesian_sde_solver.sde_solvers import euler_maruyama_pathwise
+
+JAX_KEY = jax.random.PRNGKey(1337)
+solver = ekf0
+
+gamma = 1.0
+sig = 1.0
+eps = 1.0
+alpha = 1.0
+s = 1.0
+
+
+
+
+def drift(x, t):
+    return jnp.array([[1.0 / eps, -1.0 / eps], [gamma, -1]]) @ x + jnp.array(
+        [s / eps - x[0] ** 3 / eps, alpha])
+
+
+def sigma(x, t):
+    return jnp.array([[0.0], [sig]])
+
+
+def drift(x, t):
+    def pi(x):
+        return jnp.exp(-x @ x.T / 2)
+
+    return jax.jacfwd(lambda z: jnp.log(pi(z)))(x)
+
+
+def sigma(x, t):
+    return jnp.array([[jnp.sqrt(2)]])
+
+
+x0 = jnp.ones((1,))
+x0 = jnp.ones((2,))
+
+
+def drift(x, t):
+    return jnp.array([[0.0, 1.0], [0.0, 0.0]]) @ x
+
+
+def sigma(x, t):
+    return jnp.array([[0.0], [1.0]])
+drift_s, sigma_s = to_stratonovich(drift, sigma)
+
+init = x0
+if solver in [ekf0_2, ekf1_2]:
+    P0 = jnp.zeros((x0.shape[0], x0.shape[0]))
+    init = (x0, P0)
+
+
+@partial(jnp.vectorize, signature="()->(d,n,s),(d,n,s),(d,k,l)", excluded=(1, 2, 3,))
+@partial(jax.jit, static_argnums=(1, 2, 3,))
+def experiment(delta, N, M, fine):
+
+    # special sde_solver function to solve RAM issue
+    from typing import Callable, Tuple
+
+    import jax
+    import jax.numpy as jnp
+    from numpy.typing import ArrayLike
+
+    from bayesian_sde_solver._utils import insert
+
+    def sde_solver(
+            key,
+            drift: Callable,
+            sigma: Callable,
+            x0: ArrayLike,
+            bm: Callable,
+            delta: float,
+            N: int,
+            ode_int: Callable,
+    ) -> Tuple[ArrayLike, ArrayLike, Tuple[ArrayLike]]:
+        """
+        Solve the sequence of random ODEs given a method for generating Brownian motion differentiable approximation.
+        """
+        init = x0
+        get_coeffs, eval_fn = bm()
+
+        def body(x, inp):
+            x1, x2 = x
+            key_k, t_k = inp
+            bm_key, sample_key = jax.random.split(key_k, 2)
+            coeffs_k = get_coeffs(bm_key, delta)
+            func = lambda t: eval_fn(t, delta, *coeffs_k)
+            drift_shifted = lambda z, t: drift(z, t + t_k)
+            sigma_shifted = lambda z, t: sigma(z, t + t_k)
+
+            vector_field = lambda z, t: drift(z, t + t_k) + sigma(z, t + t_k) @ jax.jacfwd(func)(t)
+            next_x = ode_int(sample_key, init=x1, vector_field=vector_field, T=delta)
+            dt = delta / fine
+            standard_incs = coeffs_k[2] * jnp.sqrt(1/dt)
+            next_x2 = euler_maruyama_pathwise(standard_incs, init=x2, drift=drift_shifted, sigma=sigma_shifted, h=dt, N=fine)
+            return (next_x, next_x2), (next_x, next_x2)
+
+        keys = jax.random.split(key, N)
+        ts = jnp.linspace(0, N * delta, N + 1)
+
+        inps = keys, ts[:-1]
+        _, samples = jax.lax.scan(body, (init, init), inps)
+        traj, traj2 = samples
+        traj = insert(traj, 0, init, axis=0)
+        traj2 = insert(traj2, 0, init, axis=0)
+        return ts, traj, traj2
+
+    keys = jax.random.split(JAX_KEY, 1_0000)
+
+    def wrapped(_key, init, vector_field, T):
+        return solver(None, init=init, vector_field=vector_field, h=T / M, N=M)
+
+    get_approx_fine = partial(_get_approx_fine, N=fine)
+
+    @jax.vmap
+    def wrapped_filter_parabola(key_op):
+        return sde_solver(
+            key=key_op,
+            drift=drift_s,
+            sigma=sigma_s,
+            x0=init,
+            bm=get_approx_fine,
+            delta=delta,
+            N=N,
+            ode_int=wrapped,
+        )
+
+    linspaces, sols, sol2 = wrapped_filter_parabola(keys)
+    if solver in [ekf0_2, ekf1_2]:
+        sols = sols[0]
+
+    @jax.vmap
+    def wrapped_euler_maruyama_piecewise(inc):
+        return euler_maruyama_pathwise(inc, init=x0, drift=drift, sigma=sigma, h=dt, N=fine * shape_incs[1])
+
+    return sols, sol2
+
+
+Ns = jnp.array([4, 8, 16, 32, 64, 128])
+deltas = jnp.array([0.25, 0.125, 0.0625, 0.03125, 0.015625, 0.0078125])
+fineDeltas = Ns ** 1
+Mdeltas = jnp.ones((len(deltas),)) * Ns ** 0
+Ndeltas = Ns
+
+folder = "./"
+solver_name = "ekf0"
+prefix = "IBM" + solver_name
+for n in range(len(Ndeltas)):
+    delta = deltas[n]
+    N = Ndeltas[n]
+    M = Mdeltas[n]
+    fine = fineDeltas[n]
+    s1, s2, incs = experiment(delta, int(N), int(M), int(fine))
+    jnp.save(f'{folder}/{prefix}_pathwise_sols_{N}_{M}', s1)
+    jnp.save(f'{folder}/{prefix}_pathwise_sols2_{N}_{fine}', s2)
